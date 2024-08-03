d2242d1e26f53ef1bda74c34831940af6bb99b2a
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2024-08-03 12:38:55 +0800

    Edited the terraform init for checks yaml file

--- provider.tf
+++ provider.tf
@@ -1,8 +1,12 @@
 terraform {
+  required_version = ">= 1.0" 
+}
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.25.0"
+      version = "~> 5.0"
     }
   }
 }
