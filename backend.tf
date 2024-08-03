a41b6016dba0d008e399000062ea03809127453e
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2024-08-03 12:25:40 +0800

    updated the backend file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce6-tfstate"              # Terraform State bucket name
-    key    = "Chrysalis-ec2-bluegreen.tfstate" # Name of your tfstate file
+    key    = "Chrysalis-blue-0" # Name of your tfstate file
     region = "ap-southeast-1"                # Terraform State bucket region
   }
 }
